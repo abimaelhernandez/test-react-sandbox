11f4a42c1f9bddaccf5062c0eeb4db13a555ba5b
Abimael <33847064+abimaelhernandez@users.noreply.github.com>
2022-10-03 19:40:13 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.scss";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1 className="app-title">Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
